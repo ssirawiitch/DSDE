a4e0237df62ad70b0e4808977a662c0fc01b7fe5
ssirawiitch <sirawitchchairuangsirikul@gmail.com>
2026-02-01 20:12:05 +0700

    add hw 3.2

--- grader/03_ml_02_2025s2/student.py
+++ grader/03_ml_02_2025s2/student.py
@@ -1,7 +1,9 @@
-
-import ... #e.g. pandas, sklearn, .....
+import pandas as pd
 import warnings # DO NOT modify this line
 from sklearn.exceptions import ConvergenceWarning # DO NOT modify this line
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 warnings.filterwarnings("ignore", category=ConvergenceWarning) # DO NOT modify this line
 
 
@@ -22,23 +24,21 @@ class BankLogistic:
 
         """
         # TODO: Paste your code here
-        pass 
+        return self.df.shape[0]
 
     def Q2(self): # DO NOT modify this line
         """
         Problem 2:
             return the tuple of numeric variables and categorical variables are presented in the dataset.
         """
-        # TODO: Paste your code here
-        pass  
+        return (self.df.select_dtypes(include=['number']).shape[1], self.df.select_dtypes(include=['object','category']).shape[1])
     
     def Q3(self): # DO NOT modify this line
         """
         Problem 3:
             return the tuple of the Class 0 (no) followed by Class 1 (yes) in 3 digits.
         """
-        # TODO: Paste your code here
-        pass 
+        return (float(round(self.df['y'].value_counts(normalize=True).get('no', 0), 3)), float(round(self.df['y'].value_counts(normalize=True).get('yes', 0), 3)))
       
     
 
@@ -47,9 +47,7 @@ class BankLogistic:
         Problem 4:
             Remove duplicate records from the data. What are the shape of the dataset afterward?
         """
-        # TODO: Paste your code here
-        
-        pass  
+        return self.df.drop_duplicates().shape
         
 
     def Q5(self): # DO NOT modify this line
@@ -65,9 +63,19 @@ class BankLogistic:
             return the tuple of shapes of X_train and X_test.
 
         """
-        # TODO: Paste your code here
-       
-        pass  
+        self.df = self.df.drop_duplicates()
+
+        self.df = self.df.replace('unknown', pd.NA)
+        cols = self.df.columns
+        for col in cols:
+            if self.df[col].value_counts(normalize=True).iloc[0] > 0.99:
+                self.df.drop([col], axis=1, inplace=True)
+        
+        X = self.df.drop(columns=['y'], axis=1)
+        y = self.df['y']
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X,y,stratify=y,random_state=0,test_size=0.3)
+
+        return (self.X_train.shape, self.X_test.shape)
 
        
     def Q6(self): 
@@ -91,10 +99,50 @@ class BankLogistic:
             return the shape of X_train.
 
         """
-        # TODO: Paste your code here
+        df_tmp = self.df.copy()
+
+        df_tmp = df_tmp.drop_duplicates()
+
+        df_tmp = df_tmp.replace('unknown', pd.NA)
+        cols = df_tmp.columns
+        for col in cols:
+            if df_tmp[col].value_counts(normalize=True).iloc[0] > 0.99:
+                df_tmp.drop([col], axis=1, inplace=True)
         
-        pass  
-    
+        X = df_tmp.drop(columns=['y'], axis=1)
+        y = df_tmp['y']
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X,y,stratify=y,random_state=0,test_size=0.3)
+
+        # Impute missing values
+        for col in self.X_train.select_dtypes(include=['number']).columns:
+            mean_val = self.X_train[col].mean()  # only form train set
+            self.X_train[col] = self.X_train[col].fillna(mean_val)
+            self.X_test[col] = self.X_test[col].fillna(mean_val)
+
+        for col in self.X_train.select_dtypes(include=['object','category']).columns:
+            mode_val = self.X_train[col].mode().iloc[0]
+            self.X_train[col] = self.X_train[col].fillna(mode_val)
+            self.X_test[col] = self.X_test[col].fillna(mode_val)
+
+        # Encode education variable
+        education_order = {
+            'illiterate': 1,
+            'basic.4y': 2,
+            'basic.6y': 3,
+            'basic.9y': 4,
+            'high.school': 5,
+            'professional.course': 6,
+            'university.degree': 7
+        }
+        self.X_train['education'] = self.X_train['education'].map(education_order)
+        self.X_test['education'] = self.X_test['education'].map(education_order)    
+
+        self.X_train = pd.get_dummies(self.X_train)
+        self.X_test = pd.get_dummies(self.X_test)
+
+        return self.X_train.shape
+
+
     def Q7(self):
         ''' Problem7: Use Logistic Regression as the model with 
             random_state=2025, 
@@ -103,10 +151,49 @@ class BankLogistic:
             Train the model using all the remaining available variables. 
             What is the macro F1 score of the model on the test data? in 3 digits
         '''
-        # TODO: Paste your code here
-        
-        pass  
-        
+        df_tmp = self.df.copy()
 
+        df_tmp = df_tmp.drop_duplicates()
 
-   
+        df_tmp = df_tmp.replace('unknown', pd.NA)
+        cols = df_tmp.columns
+        for col in cols:
+            if df_tmp[col].value_counts(normalize=True).iloc[0] > 0.99:
+                df_tmp.drop([col], axis=1, inplace=True)
+        
+        X = df_tmp.drop(columns=['y'], axis=1)
+        y = df_tmp['y']
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X,y,stratify=y,random_state=0,test_size=0.3)
+
+        # Impute missing values
+        for col in self.X_train.select_dtypes(include=['number']).columns:
+            mean_val = self.X_train[col].mean()  # only form train set
+            self.X_train[col] = self.X_train[col].fillna(mean_val)
+            self.X_test[col] = self.X_test[col].fillna(mean_val)
+
+        for col in self.X_train.select_dtypes(include=['object','category']).columns:
+            mode_val = self.X_train[col].mode().iloc[0]
+            self.X_train[col] = self.X_train[col].fillna(mode_val)
+            self.X_test[col] = self.X_test[col].fillna(mode_val)
+
+        # Encode education variable
+        education_order = {
+            'illiterate': 1,
+            'basic.4y': 2,
+            'basic.6y': 3,
+            'basic.9y': 4,
+            'high.school': 5,
+            'professional.course': 6,
+            'university.degree': 7
+        }
+        self.X_train['education'] = self.X_train['education'].map(education_order)
+        self.X_test['education'] = self.X_test['education'].map(education_order)    
+
+        self.X_train = pd.get_dummies(self.X_train)
+        self.X_test = pd.get_dummies(self.X_test)
+
+        model = LogisticRegression(random_state=2025, class_weight='balanced', max_iter=500)
+        model.fit(self.X_train, self.y_train)
+        y_pred = model.predict(self.X_test)
+        report = classification_report(self.y_test, y_pred, output_dict=True)
+        return round(report['macro avg']['f1-score'],2)
